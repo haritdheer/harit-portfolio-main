01edd6fd37c8dff640bcca39dc323ca9ee70c0cb
Harit <97253880+haritdheer@users.noreply.github.com>
2025-08-23 20:12:04 +0530

    Update about_page_right.jsx
    updated about page

--- src/components/about_page/about_page_right.jsx
+++ src/components/about_page/about_page_right.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import CustomContainer from "../../containers/customContainer";
-import Photo from "../../assets/Photo.jpg";
+// import Photo from "../../assets/Photo.jpg";
 import "./style.css";
 
 const AboutPageRight = () => {
   return (
     <CustomContainer className={"flex w-1/2 justify-center items-start"}>
       <CustomContainer className={"h-80 w-56 wrapper"}>
-        <img src={Photo} className="w-full h-full" />
+{/*         <img src={Photo} className="w-full h-full" /> */}
       </CustomContainer>
       {/* <div className="wrapper_outline"></div> */}
     </CustomContainer>
